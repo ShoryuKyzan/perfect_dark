9ed0f5b193a95a895f9e8f6fe4123f5cca4ff0ab
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2025-05-22 23:31:20 -0400

    flip controller y offset from hmd, so that it is correct

--- src/game/bondgun.c
+++ src/game/bondgun.c
@@ -7571,7 +7571,7 @@ void bgunApplyVRControllerPos(struct hand *hand, s32 handnum, Mtxf *dest) {
 		float controller_pos[3];
 		vrGetHMDPosition(hmd_pos);
 		controller_pos[0] = (hmd_pos[0] - controllerMatrix.m[3][0]);
-		controller_pos[1] = (hmd_pos[1] - controllerMatrix.m[3][1]);
+		controller_pos[1] = (controllerMatrix.m[3][1] - hmd_pos[1]); // for some reason its wrong unless i flip this
 		controller_pos[2] = (hmd_pos[2] - controllerMatrix.m[3][2]);
 		// scale
 		controller_pos[0] *= vrGetControllerWorldScaleFactor();
